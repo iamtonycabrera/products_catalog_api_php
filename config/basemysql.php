3e0fa140441698c3caa35ecb60d25f618131ad97
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-11 22:05:49 +0200

    Configured connection to database

---
+++ config/basemysql.php
@@ -0,0 +1,29 @@
+<?php
+
+    class Basemysql {
+
+        // Parametros de base de datos
+        private $host = 'localhost';
+        private $db_name = 'catalogo_productos';
+        private $username = 'root';
+        private $password = '';
+        private $conn;
+        
+        // Conexion a la db
+        public function connect(){
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+            } catch (PDOException $e) {
+                echo "Error en la conexion: " . $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+    }
+
+    $conenction = new Basemysql();
+    $conenction->connect();
+
+?>
